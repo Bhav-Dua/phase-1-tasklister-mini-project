d199c807f11446328a44c5b0f9126d7088dd2aaa
Bhavya Dua <50784193+Bhav-Dua@users.noreply.github.com>
2022-11-08 16:27:27 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', createTask);
+})
+
+function createTask(e){
+  e.preventDefault();
+
+  const todo = e.target.elements['new-task-description'].value;
+  const li = document.createElement('li');
+  const btn = document.createElement('button');
+
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  li.textContent = `${todo} `;
+
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+  
+  e.target.reset();
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
